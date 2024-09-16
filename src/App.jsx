439e6c259d833cb3a4e0b591656ea4cfe469b93d
TLAMHutto <keatonhutto8@gmail.com>
2024-09-16 00:07:13 -0500

    vercel-analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Experience } from "./components/Experience";
 import { UI } from "./components/UI";
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <>
+    <Analytics/>
       <UI />
       <Loader />
       <Canvas shadows camera={{ position: [-0.5, 1, 4], fov: 45 }}>
